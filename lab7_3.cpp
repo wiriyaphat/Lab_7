b6f309dcb98fdcd95f7b640750e42f6372a677d2
Thiw <wiriyaphat6789@gmail.com>
2025-12-12 16:14:06 +0700

    lab7_3.cpp complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if (x > 'A' && x <= 'Z'){return x-1;}
+    if (x == 'A'){return 'Z';}
+    return '0';
 }
 
 int main(){
